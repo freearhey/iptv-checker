10ca6d7ada52785efeb7fda48f3f5c0894a021e6
freearhey <free.arhey@gmail.com>
2019-09-28 02:59:22 +0300

    Bump to v0.10.1

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const dateFormat = require('dateformat')
 let seedFile
 
 argv
-  .version('0.10.0', '-v, --version')
+  .version('0.10.1', '-v, --version')
   .usage('[options] <file>')
   .option('-o, --output [output]', 'Path to output file')
   .option('-t, --timeout [timeout]', 'Set the number of milliseconds before the request times out')
