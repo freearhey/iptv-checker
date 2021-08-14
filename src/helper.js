089cf67a414cc5eb5124f01e66bf284aa6717855
Aleksandr Statciuk <free.arhey@gmail.com>
2021-08-14 17:27:50 +0300

    Update helper.js

--- src/helper.js
+++ src/helper.js
@@ -48,7 +48,7 @@ async function parsePlaylist(input) {
   return parse(data)
 }
 
-function parseStdout(output, item) {
+function parseError(output, item) {
   const url = item.url
   const line = output.split('\n').find(l => {
     return l.indexOf(url) === 0
@@ -61,6 +61,41 @@ function parseStdout(output, item) {
   return line.replace(`${url}: `, '')
 }
 
+function parseStderr(stderr) {
+  const requests = stderr
+    .split('\r\n\n')
+    .map(parseRequest)
+    .filter(l => l)
+
+  return { requests }
+}
+
+function parseRequest(string) {
+  const urlMatch = string.match(/\Opening \'(.*)\' for reading/)
+  const url = urlMatch ? urlMatch[1] : null
+  if (!url) return null
+  const requestMatch = string.match(/request: (.|[\r\n])+/gm)
+  const request = requestMatch ? requestMatch[0] : null
+  if (!request) return null
+  const arr = request
+    .split('\n')
+    .map(l => l.trim())
+    .filter(l => l)
+  const methodMatch = arr[0].match(/request: (GET|POST)/)
+  const method = methodMatch ? methodMatch[1] : null
+  arr.shift()
+  if (!arr) return null
+  const headers = {}
+  arr.forEach(line => {
+    const parts = line.split(': ')
+    if (parts && parts[1]) {
+      headers[parts[0]] = parts[1]
+    }
+  })
+
+  return { method, url, headers }
+}
+
 function checkItem(item) {
   const { config, logger } = this
 
@@ -69,12 +104,14 @@ function checkItem(item) {
   logger.debug(`EXECUTING: "${command}"`)
 
   return execAsync(command, { timeout: config.timeout })
-    .then(({ stdout }) => {
-      if (stdout && isJSON(stdout)) {
+    .then(({ stdout, stderr }) => {
+      if (stdout && isJSON(stdout) && stderr) {
         const metadata = JSON.parse(stdout)
         if (!metadata.streams.length) {
           return { ok: false, reason: 'No streams found' }
         }
+        const results = parseStderr(stderr)
+        metadata.requests = results.requests
 
         return { ok: true, metadata }
       }
@@ -82,7 +119,7 @@ function checkItem(item) {
       return { ok: false, reason: 'Parsing error' }
     })
     .catch(err => {
-      const reason = parseStdout(err.message, item)
+      const reason = parseError(err.message, item)
 
       return { ok: false, reason }
     })
@@ -96,9 +133,10 @@ function buildCommand(item, config) {
   let args = [
     `ffprobe`,
     `-of json`,
-    `-v error`,
+    `-v debug`,
     `-hide_banner`,
     `-show_streams`,
+    `-show_format`,
   ]
 
   if (referrer.length) {
