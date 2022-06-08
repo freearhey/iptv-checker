b37139298d3b5dbe8050efabc667525c2133fa0a
Aleksandr Statciuk <github@oiya.xyz>
2022-06-08 19:09:53 +0300

    Fix linter issues

--- src/cache.js
+++ src/cache.js
@@ -4,17 +4,17 @@ module.exports.check = check
 let cache = new Set()
 
 function add({ url }) {
-	let id = hashUrl(url)
+  let id = hashUrl(url)
 
-	cache.add(id)
+  cache.add(id)
 }
 
 function check({ url }) {
-	let id = hashUrl(url)
+  let id = hashUrl(url)
 
-	return cache.has(id)
+  return cache.has(id)
 }
 
 function hashUrl(u) {
-	return Buffer.from(u).toString(`hex`)
+  return Buffer.from(u).toString(`hex`)
 }
