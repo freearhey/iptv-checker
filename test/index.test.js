17f86e1db3b97bf5e3c85c79a7384fec08ce1b3e
Aleksandr Statciuk <github@oiya.xyz>
2023-02-02 20:05:02 +0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -31,7 +31,7 @@ test(`Should process a playlist URL`, done => {
 test(`Should process a stream URL`, done => {
   const url = 'https://live-hls-web-aja.getaj.net/AJA/index.m3u8'
   checker
-    .checkStream({ url, timeout: 10000 })
+    .checkStream({ url, timeout: 50000 })
     .then(results => {
       expect(results.status.ok).toBeTruthy()
       done()
